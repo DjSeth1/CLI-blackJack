f1b7d4efe99ba33023bea06307e65920db433908
Divij Seth <divijseth@Divijs-MBP.hub>
2022-09-24 21:06:51 +1200

    comments about what needs to be tested

--- test_classes.py
+++ test_classes.py
@@ -1,5 +1,19 @@
 
 
+#class deck - test if it returns a full list of cards
+#test if shuffled deck returns a random list of cards 
+#test if deal returns one card from deck
+
+#test class hand
+# test if it returns a printed card of show_card function
+
+#test player class
+#test if player stores name
+#test if player stores a balance and check negative values to see if it raises the errors outlined
+#test if win bet doubles bet value 
+#tes if win bet adds to balance
+#test if lose bet minuses from balance value
+
 
 
 
---
+++ test_main.py
@@ -0,0 +1,11 @@
+# test take bet and raise error when value is not an integer
+
+#test take bet and raise an error when bet is more than balance and work when bet is lower than balance
+
+#test hit or stand if input H it works if input S it works and raises errror when not h or s 
+
+#test play again, check if balance is more than 0 it prompts to start new game
+#test play again to raise error when y or n is not pressed 
+
+
+#test end game logic
