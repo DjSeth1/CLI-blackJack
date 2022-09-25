b600263ab63bd43a7c1e384bfad9fc519bee94b6
Divij Seth <divijseth@Divijs-MBP.hub>
2022-09-25 19:13:18 +1300

    added bash scripting for file execution

---
+++ src/main.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Welcome to Blackjack! Installing...."
+
+echo "Checking for virtual environment"
+FILE=.venv
+if [ -d "$FILE" ]; then
+    echo "Virtual environment exists"
+else
+    echo "Creating virtual environment"
+    python3 -m venv .venv
+    source .venv/bin/activate
+fi
+
+echo "Checking if following dependecies are installed"
+
+pip install -r requirements.txt
+
+echo "All python packages are installed, running application"
+
+python3 main.py
